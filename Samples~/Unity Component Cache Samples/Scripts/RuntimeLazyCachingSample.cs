66313e62954f4d787e4f3a4b758fde4bcdb873b6
Rocky Hong <contact@rockyhong.com>
2023-12-21 14:46:55 +0800

    Update Samples

---
+++ Samples~/Unity Component Cache Samples/Scripts/RuntimeLazyCachingSample.cs
@@ -0,0 +1,26 @@
+using UnityGetComponentCache;
+using UnityEngine;
+
+namespace UnityGetComponentCache.Samples
+{
+    public class RuntimeLazyCachingSample : LazyGetComponentCacheBehaviour
+    {
+        private Transform cachedTransform => GetComponentCache<Transform>();
+        private TextMesh cachedTextMesh => GetComponentCache<TextMesh>();
+    
+        private void Update()
+        {
+            float someValue = Mathf.Sin(Time.time);
+        
+            // Demo: Do something with the cached transform
+            var position = cachedTransform.position;
+            Vector3 somePosition = new Vector3(someValue, position.y, position.z);
+            Quaternion someRotation = Quaternion.Euler(someValue * 45f, 0, 0);
+            cachedTransform.SetPositionAndRotation(somePosition, someRotation);
+
+            // Demo: Do something with the cached text mesh
+            cachedTextMesh.text = $"{name} : {someValue:F2}";
+            cachedTextMesh.color=Color.Lerp(Color.red, Color.blue, someValue);
+        }
+    }
+}    
